6f3f813fb2cf720ed1194d879d3ae53259fb5d4f
nRomanw <softwaremail1@web.de>
2023-11-26 11:22:38 +0100

    update setup.sh with i3

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-mkdir -p ~/.config/tmux ~/.config/nvim ~/.config/alacritty
+mkdir -p ~/.config/tmux ~/.config/nvim ~/.config/alacritty ~/.config/i3
 
 ln ~/.dotfiles/alacritty.yml ~/.config/alacritty/
 ln ~/.dotfiles/tmux.conf ~/.config/tmux/
 ln ~/.dotfiles/init.lua ~/.config/nvim/
+ln ~/.dotfiles/config ~/.config/i3/
