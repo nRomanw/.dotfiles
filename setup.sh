f0140dd2541fd94d08f3f4264ca5e07d1db62ca8
nRomanw <softwaremail1@web.de>
2023-11-26 14:55:59 +0100

    add sourcecodepro fonts

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
-mkdir -p ~/.config/tmux ~/.config/nvim ~/.config/alacritty ~/.config/i3
+mkdir -p ~/.config/tmux ~/.config/nvim ~/.config/alacritty ~/.config/i3 ~/.fonts
 
 ln ~/.dotfiles/alacritty.yml ~/.config/alacritty/
 ln ~/.dotfiles/tmux.conf ~/.config/tmux/
 ln ~/.dotfiles/init.lua ~/.config/nvim/
 ln ~/.dotfiles/config ~/.config/i3/
+
+cd ~/.fonts
+git clone https://github.com/nRomanw/source-code-pro.git 
+cd ~
