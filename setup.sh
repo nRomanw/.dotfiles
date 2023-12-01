4e24253057987d584521673abfe55978a5c1f85f
nRomanw <softwaremail1@web.de>
2023-12-01 16:54:23 +0100

    bug fixes

--- setup.sh
+++ setup.sh
@@ -14,5 +14,8 @@ git clone https://github.com/nRomanw/source-code-pro.git ~/.fonts
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 brew install gcc neovim
+
 source ~/.bashrc
