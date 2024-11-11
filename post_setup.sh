69051d174f609e51e74e231111be9ea415ea6ad4
nRomanw <softwaremail1@web.de>
2024-11-11 11:08:04 +0100

    create post_setup.sh

---
+++ post_setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+nix-env -iA nixpkgs.zathura nixpkgs.neovim nixpkgs.lazygit nixpkgs.sqlite nixpkgs.sqlitebrowser
+
+
+sudo flatpak install flathub org.torproject.torbrowser-launcher -y
+sudo flatpak install flathub com.brave.Browser -y
+sudo flatpak install flathub network.loki.Session -y
+sudo flatpak install flathub io.github.ungoogled_software.ungoogled_chromium -y
+sudo flatpak install flathub org.gnome.Evince -y
+sudo flatpak install flathub io.gitlab.librewolf-community -y
+sudo flatpak install flathub com.calibre_ebook.calibre -y
+sudo flatpak install flathub org.mozilla.Thunderbird -y
+sudo flatpak install flathub com.logseq.Logseq -y
+sudo flatpak install flathub org.mozilla.firefox -y
+sudo flatpak install flathub org.libreoffice.LibreOffice -y
+sudo flatpak install flathub org.gnome.Maps -y
+sudo flatpak install flathub com.github.tchx84.Flatseal -y
+sudo flatpak install flathub com.vscodium.codium -y
+
+tldr -u
+
+cargo install tmux-sessionizer
+cargo install zoxide
+cargo install nu
+cargo install starship
+cargo install atuin
+
+#autoinstall latest go and nodejs
