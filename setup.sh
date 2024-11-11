cafa239757725459933341b409e2ec56b332dc0d
nRomanw <softwaremail1@web.de>
2024-11-11 12:24:51 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -27,8 +27,8 @@ ln ~/.dotfiles/nushell/config.nu ~/.config/nushell
 ln ~/.dotfiles/nushell/env.nu ~/.config/nushell
 ln ~/.dotfiles/starship.toml ~/.config/starship.toml
 
-git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
-make -C ble.sh install PREFIX=~/.local
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git ~/workspaces/clones
+make -C ~/workspaces/clones/ble.sh install PREFIX=~/.local
 
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
