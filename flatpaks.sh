f192c536df2680266688f9475a698a51683cc39c
nRomanw <softwaremail1@web.de>
2025-03-30 15:42:13 +0200

    rename post_setup.sh to flatpak.sh

---
+++ flatpaks.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+sudo flatpak install flathub com.belmoussaoui.Authenticator -y
+sudo flatpak install flathub org.torproject.torbrowser-launcher -y
+sudo flatpak install flathub com.brave.Browser -y
+sudo flatpak install flathub network.loki.Session -y
+sudo flatpak install flathub io.github.ungoogled_software.ungoogled_chromium -y
+sudo flatpak install flathub org.gnome.Evince -y
+sudo flatpak install flathub io.gitlab.librewolf-community -y
+sudo flatpak install flathub com.calibre_ebook.calibre -y
+sudo flatpak install flathub com.logseq.Logseq -y
+sudo flatpak install flathub org.libreoffice.LibreOffice -y
+sudo flatpak install flathub org.gnome.Maps -y
+sudo flatpak install flathub com.github.tchx84.Flatseal -y
+sudo flatpak install flathub com.vscodium.codium -y
+sudo flatpak install flathub eu.betterbird.Betterbird -y
+
