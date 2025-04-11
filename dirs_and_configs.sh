f8d2c121b4fa48abb180ffcbbf9ac5219811b1d3
nRomanw <softwaremail1@web.de>
2025-04-11 19:32:44 +0200

    update

--- dirs_and_configs.sh
+++ dirs_and_configs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/tmux ~/.config/kitty ~/.config/alacritty ~/.config/newsboat  ~/.local/share/fonts  ~/.config/dunst ~/.config/lazygit ~/.config/nvim ~/.config/nushell ~/.config/dconf
+mkdir -p ~/.config/tmux ~/.config/kitty ~/.config/alacritty ~/.config/newsboat  ~/.local/share/fonts  ~/.config/dunst ~/.config/lazygit ~/.config/nvim ~/.config/nushell ~/.config/dconf ~/.config/ghostty ~/.config/nvim
+
+git clone https://github.com/nRomanw/my_fork_of_kickstart.nvim.git ~/.config/nvim
 
 mv ~/.bashrc ~/.bashrc.sysdefault
 ln ~/.dotfiles/.bashrc ~/
@@ -14,3 +16,5 @@ ln ~/.dotfiles/dunst/dunstrc ~/.config/dunst
 ln ~/.dotfiles/newsboat/config ~/.config/newsboat
 ln ~/.dotfiles/nushell/config.nu ~/.config/nushell
 ln ~/.dotfiles/nushell/env.nu ~/.config/nushell
+ln ~/.dotfiles/starship.toml ~/.config
+ln ~/.dotfiles/ghostty/config ~/.config/ghostty
