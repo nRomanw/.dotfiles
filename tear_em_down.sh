37df80685e7542f4b726f1205039a31b350e142c
nRomanw <softwaremail1@web.de>
2025-04-11 19:33:09 +0200

    remove all symlinks and their repos

---
+++ tear_em_down.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -rfv ~/.config/tmux ~/.config/kitty ~/.config/alacritty ~/.config/newsboat  ~/.local/share/fonts  ~/.config/dunst ~/.config/lazygit ~/.config/nushell ~/.config/dconf ~/.config/ghostty
+rm ~/.strship.toml
